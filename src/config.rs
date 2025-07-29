7feb17873ddfacbeda88be4850d5ddfcac4b2054
Krist309 <krist@giftexco.com>
2025-07-29 17:51:31 +0800

    修改默认服务器和key

--- src/config.rs
+++ src/config.rs
@@ -100,8 +100,8 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["rs-ny.rustdesk.com"];
-pub const RS_PUB_KEY: &str = "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["sh.giftexco.com"];
+pub const RS_PUB_KEY: &str = "X6QeEkuaqeWqvHDO+fLyqIlGN32Xhcm7Mv2Gs8A3b38=";
 
 pub const RENDEZVOUS_PORT: i32 = 21116;
 pub const RELAY_PORT: i32 = 21117;
